2f1cb5923319fd36d6a6ba96907f1b3c96bb868c
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-08-24 09:17:58 +0300

    import products from products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,5 @@
 import { cart } from '../data/cart.js';
-
+import { products } from '../data/products.js';
 let productsHTML = '';
 //loading products from data/product.js
 products.forEach((product) => {
