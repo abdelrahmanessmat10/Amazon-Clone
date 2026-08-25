a8a56bb1ce45330462f0f345d68abde1609cc851
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-08-25 14:54:34 +0300

    Save data and generate HTML for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -12,3 +12,12 @@ export const deliveryOptions = [{
   deliveryTime: 1,
   priceCents: 999
 }];
+
+export function getDeliveryOption(deliveryOptionId) {
+  let deliveryOption;
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId)
+      deliveryOption = option;
+  });
+  return deliveryOption || deliveryOptions[0];
+}
