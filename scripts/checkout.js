8f8d2dd83db3f4183c52f537719376cf4147fcc4
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-08-25 09:30:19 +0300

    save data and generate HTML for delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,12 @@
 import { cart, deleteFromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js'
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
+import { deliveryOptions } from '../data/deliveryOptions.js'
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+
+
 
 let cartSummaryHTML = '';
 
@@ -13,13 +19,22 @@ cart.forEach((cartItem) => {
       matchingProduct = product;
   });
 
-  console.log(matchingProduct);
+  const deliveryOptionId = cartItem.deliveryOptionId;
+
+  let deliveryOption;
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId)
+      deliveryOption = option;
+  });
+  const today = dayjs();
+  const deliveryDate = today.add(deliveryOption.deliveryTime, 'days');
+  const dateString = deliveryDate.format('dddd, MMMM D');
 
 
   cartSummaryHTML += `
   <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${dateString}
       </div>
 
       <div class="cart-item-details-grid">
@@ -49,39 +64,7 @@ cart.forEach((cartItem) => {
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionsHTML(matchingProduct, cartItem)}
         </div>
       </div>
     </div>
@@ -102,3 +85,34 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
 
   });
 });
+
+function deliveryOptionsHTML(matchingProduct, cartItem) {
+  let html = '';
+  deliveryOptions.forEach((deliveryOption) => {
+    const today = dayjs();
+    const deliveryDate = today.add(deliveryOption.deliveryTime, 'days');
+    const dateString = deliveryDate.format('dddd, MMMM D');
+
+    const priceSrting = deliveryOption.priceCents !== 0 ? `$${formatCurrency(deliveryOption.priceCents)} -` : 'FREE';
+
+    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+    html += `
+              <div class="delivery-option">
+                <input type="radio" 
+                ${isChecked ? 'checked' : ''}
+                class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+                <div>
+                  <div class="delivery-option-date">
+                    ${dateString}
+                  </div>
+                  <div class="delivery-option-price">
+                    $${priceSrting} Shipping
+                  </div>
+                </div>
+              </div>
+  `;
+  });
+
+  return html;
+}
