8a4b789441253122f85cc540583648bdb2e8b915
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-08-26 10:22:25 +0300

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,8 +3,8 @@ import { products, getProduct } from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js'
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js'
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
+import { renderPaymentSummary } from './paymentSummary.js';
+
 
 
 export function renderOrderSummary() {
@@ -107,8 +107,11 @@ export function renderOrderSummary() {
       deleteFromCart(productId);
 
 
+
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
+
+      renderPaymentSummary();
 
     });
   });
@@ -118,6 +121,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
 
     });
   });
