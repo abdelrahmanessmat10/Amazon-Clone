a208756afc8d3969e4838b4ea95a25cf696acfd3
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-08-26 16:48:24 +0300

    Rename test-jasmine folder to tests

--- tests/tests-simple/moneyTest.js
+++ tests/tests-simple/moneyTest.js
@@ -1,5 +1,5 @@
 
-import { formatCurrency } from "../scripts/utils/money.js";
+import { formatCurrency } from "../../scripts/utils/money.js";
 console.log('test suite: formatCurrency');
 
 console.log('converts cents into dollars');
