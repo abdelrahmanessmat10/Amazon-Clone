98cfa3d9fecfe897103cd4b3611e6800b7a9aaba
abdelrahmanessmat10 <23-101348@students.eui.edu.eg>
2026-08-28 08:43:39 +0300

    Add calculateCartQuantity to cart.js

--- data/cart.js
+++ data/cart.js
@@ -72,3 +72,13 @@ export function updateDeliveryOption(productId, deliveryOptionId) {
   saveToStorage();
 
 }
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  return cartQuantity;
+}
